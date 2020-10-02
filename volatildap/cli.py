4a6a76a08adcbe6787e91a90bdb1b907c6bff183
Raphaël Barrois <raphael.barrois@paylead.fr>
2020-10-02 17:51:33 +0200

    Add a simple command line.

---
+++ volatildap/cli.py
@@ -0,0 +1,49 @@
+import argparse
+import sys
+
+import toml
+
+from . import LOCALHOST_TLS_CONFIG
+from . import server
+
+
+def launch(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', default='', help="Port to listen on; empty for a dynamic port")
+    parser.add_argument('--suffix', default=server.DEFAULT_SUFFIX, help="LDAP suffix")
+    parser.add_argument('--rootdn', default=server.DEFAULT_ROOTDN, help="Distinguished Name of LDAP admin user")
+    parser.add_argument('--rootpw', default='', help="Password of LDAP admin user")
+    parser.add_argument('--debug', default=server.DEFAULT_SLAPD_DEBUG, type=int, help="slapd debug level")
+    parser.add_argument('--initial', type=argparse.FileType('r'), help="Load initial objects from the provided TOML file")
+    parser.add_argument('--tls', action='store_true', help="Enable TLS")
+
+    args = parser.parse_args(argv)
+    if args.initial:
+        initial = toml.load(args.initial)
+    else:
+        initial = {}
+    if args.tls:
+        tls_config = LOCALHOST_TLS_CONFIG
+    else:
+        tls_config = None
+
+    instance = server.LdapServer(
+        suffix=args.suffix,
+        rootdn=args.rootdn,
+        rootpw=args.rootpw,
+        port=int(args.port) if args.port else None,
+        slapd_debug=args.debug,
+        initial_data=initial,
+        tls_config=tls_config,
+    )
+    instance.start()
+    sys.stdout.write("LDAP server listening on %s\n" % instance.uri)
+    sys.stdout.write("Credentials: %s / %s\n" % (instance.rootdn, instance.rootpw))
+    try:
+        instance.wait()
+    finally:
+        instance.stop()
+
+
+if __name__ == '__main__':
+    launch(sys.argv[1:])
