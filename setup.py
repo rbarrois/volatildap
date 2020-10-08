0e0f29df4ca50e4671e1e06bffb1088bf73de7cb
Raphaël Barrois <raphael.barrois@polytechnique.org>
2020-10-08 12:57:33 +0200

    packaging: Move all metadata to setup.cfg.

--- setup.py
+++ setup.py
@@ -2,64 +2,7 @@
 # -*- coding: utf-8 -*-
 # This software is distributed under the two-clause BSD license.
 
-from __future__ import unicode_literals
-
-import codecs
-import os
-import re
-
-from setuptools import find_packages, setup
-
-root_dir = os.path.abspath(os.path.dirname(__file__))
-
-
-def get_version(package_name):
-    version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
-    package_components = package_name.split('.')
-    init_path = os.path.join(root_dir, *(package_components + ['version.py']))
-    with codecs.open(init_path, 'r', 'utf-8') as f:
-        for line in f:
-            match = version_re.match(line[:-1])
-            if match:
-                return match.groups()[0]
-    return '0.1.0'
-
-
-PACKAGE = 'volatildap'
-
+from setuptools import setup
 
 setup(
-    name=PACKAGE,
-    version=get_version(PACKAGE),
-    description="Temporary slapd launcher for testing purposes",
-    long_description=''.join(codecs.open('README.rst', 'r', 'utf-8').readlines()),
-    author="Raphaël Barrois",
-    author_email="raphael.barrois+%s@polytechnique.org" % PACKAGE,
-    license="BSD",
-    keywords=['ldap', 'test', 'openldap', 'slapd'],
-    url="https://github.com/rbarrois/%s/" % PACKAGE,
-    download_url="https://pypi.python.org/pypi/%s/" % PACKAGE,
-    packages=find_packages(exclude=['tests*']),
-    platforms=["OS Independent"],
-    zip_safe=False,
-    install_requires=[
-    ],
-    setup_requires=[
-        'setuptools>=0.8',
-    ],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: Unix",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Testing",
-        "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",
-    ],
-    test_suite='tests',
 )
